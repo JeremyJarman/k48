f0ec5a2a7d6bded3bc197e2c13a285cbe05c7c6e
JeremyJarman <j.jarman.zim@gmail.com>
2025-01-24 19:38:54 +0100

    Changed the Wortschatz screen

--- lib/wortschatz_page.dart
+++ lib/wortschatz_page.dart
@@ -5,37 +5,56 @@ class WortschatzPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: Text('Wortschatz'),
+        backgroundColor: Theme.of(context).primaryColor.withOpacity(0.3),
+        title: Text(
+          'Wortschatz',
+          style: TextStyle(color: Colors.white),
+        ),
+        iconTheme: IconThemeData(color: Colors.white), // Set back arrow color to white
       ),
-      body: ListView(
+      body: Stack(
         children: [
-          ListTile(
-            title: Text('Menschen'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => GameplayScreen(dataset: 'WordLists_01.csv', title: 'Menschen')),
-              );
-            },
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/galaxy.jpg'), // Use the same background image as the home page
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-          ListTile(
-            title: Text('Stationen im Leben'),
-            onTap: () {
-              // TODO: Unlock and navigate to this dataset
-            },
-          ),
-          ListTile(
-            title: Text('Wohnen'),
-            onTap: () {
-              // TODO: Unlock and navigate to this dataset
-            },
-          ),
-          ListTile(
-            title: Text('Freizeit und Kultur'),
-            onTap: () {
-              // TODO: Unlock and navigate to this dataset
-            },
+          ListView(
+            padding: EdgeInsets.only(top: kToolbarHeight + 20), // Add spacing between the top app bar and the first option
+            children: [
+              ListTile(
+                title: Text('01. Menschen', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GameplayScreen(dataset: 'WordLists_01.csv', title: 'Menschen')),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('02. Stationen im Leben', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  // TODO: Unlock and navigate to this dataset
+                },
+              ),
+              ListTile(
+                title: Text('03. Wohnen', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  // TODO: Unlock and navigate to this dataset
+                },
+              ),
+              ListTile(
+                title: Text('04. Freizeit und Kultur', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  // TODO: Unlock and navigate to this dataset
+                },
+              ),
+            ],
           ),
         ],
       ),
